b0b63c20b2d02fd1b6927bc2969a3509f23f5cf8
Edward Ng <66270109+edng5@users.noreply.github.com>
2023-11-23 21:37:14 -0500

    made code more modular

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 const { Client, IntentsBitField } = require('discord.js');
 require("dotenv").config();
 const { OpenAI } = require("openai");
-const { clearInterval } = require('timers');
-
-var Scraper = require('images-scraper');
-
-const images_scraper = new Scraper({
-  puppeteer: {
-    headless: true,
-  },
-});
-
-const stopwords = ['cortex','image','show','generate','i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now'] 
-
-function remove_stopwords(str) {
-  res = []
-  words = str.split(' ')
-  for(i=0;i<words.length;i++) {
-     word_clean = words[i].split(".").join("")
-     if(!stopwords.includes(word_clean)) {
-         res.push(word_clean)
-     }
-  }
-  return(res.join(' '))
-}  
+const { scrapeImage } = require("./commands/imageScraper.js")
 
 // Initialize OpenAI using API Key
 const openai = new OpenAI({
@@ -57,15 +35,15 @@ client.on('messageCreate', async (message) => {
       message.content.includes('generate picture') || message.content.includes('show me')){
       const image_query = message.content
       if (!image_query) return message.channel.send("Sorry I can't find what you are looking for.");
-      message.content = remove_stopwords(message.content)
-      let i = 10
-      const image_results = await images_scraper.scrape(message.content, i);
-      let x = parseInt(Math.random() * (i - 1));
       message.channel.send("Sure! Here you go!");
-      message.channel.send(image_results[x].url);
+      message.channel.send(await scrapeImage(image_query));
       clearInterval(sendTypingInterval);
       return;
     }
+
+    // if (message.content.includes('make announcement') || message.content.includes('set reminder')){
+    //   let seconds, event = scheduleParser(message.content)
+    // }
     
     let conversation = [];
     conversation.push({"role": "system", "content": 'Chat GPT is a friendly chatbot named Cortex'});
